45d39a9bbf6c80459a73df8c046202906ab926cf
Daniel Isaac Khan Ramiro <di.khan.r@gmail.com>
2016-12-24 19:10:09 +0000

    set head origin and create symbolic-ref to head
    Without this travis-ci will complain throwing the following exception:
    
    [ERROR] Failed to execute goal org.apache.maven.plugins:maven - release - plugin:2.5.3:prepare (default-cli) on project hello-world-cicd: An error is occurred in the checkin process: Exception while executing SCM command. Detecting the current branch failed: fatal: ref HEAD is not a symbolic ref -> [Help 1]

--- travis-ci/deploy.sh
+++ travis-ci/deploy.sh
@@ -17,6 +17,11 @@ if [[ "$TRAVIS_COMMIT_DESCRIPTION" != *"maven-release-plugin"* ]];then
         git config --global user.email $GITHUB_EMAIL
         git config --global user.name $GITHUB_USERNAME
 
+        git remote set-head origin $TRAVIS_BRANCH
+        git show-ref --head
+        git symbolic-ref HEAD refs/heads/$TRAVIS_BRANCH
+        git symbolic-ref HEAD
+
         mvn --batch-mode release:clean release:prepare -Dusername=$GITHUB_USERNAME -Dpassword=$GITHUB_PASSWORD
         mvn release:perform --settings travis-ci/settings.xml
     fi
